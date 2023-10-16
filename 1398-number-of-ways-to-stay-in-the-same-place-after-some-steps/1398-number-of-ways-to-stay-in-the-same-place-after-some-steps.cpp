7cb12758060a2210feb608b08d1509a9ab5e7565
Shubham kashyap <110350667+Shubhamkyup@users.noreply.github.com>
2023-10-16 12:35:33 +0530

    Time: 23 ms (51.41%), Space: 10.4 MB (61.82%) - LeetHub

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-
     int numWays(int steps, int arrLen) {
-        int M = 1e9+7;
-        vector<vector<int>> dp(steps/2+1,vector<int>(steps+1,-1));
-        function<long long(int,int)> dfs = [&](int pos,int steps)->long long{
-                if(pos < 0 ||  pos > arrLen -1 || pos > steps) return 0;
-                if(steps == 0) return pos  == 0;
-                if(dp[pos][steps]!=-1) return dp[pos][steps];
-                return  dp[pos][steps] = (
-                    dfs(pos - 1, steps - 1) % M  + dfs(pos , steps-1) % M +
-                    dfs(pos + 1, steps - 1) % M
-                )%M;
-        };
-        return dfs(0,steps)%M;
+        int m = steps;
+        int n = min(steps / 2 + 1, arrLen);
+        
+        vector<vector<int>> dp(m + 1, vector<int>(n, 0));
+        dp[0][0] = 1;
+        
+        int mod = 1000000007; 
+        for (int i = 1; i <= m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j > 0) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % mod;
+                }
+                if (j < n - 1) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j + 1]) % mod;
+                }
+            }
+        }
+        
+        return dp[m][0];
     }
 };
