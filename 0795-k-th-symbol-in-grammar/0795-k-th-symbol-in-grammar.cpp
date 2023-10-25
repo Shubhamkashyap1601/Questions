9204b763e8c09292bec4d75b82fc53575641f4f9
Shubham kashyap <110350667+Shubhamkashyap1601@users.noreply.github.com>
2023-10-25 10:54:26 +0530

    Time: 2 ms (51.37%), Space: 6.2 MB (65.8%) - LeetHub

---
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if(n==1) return 0;
+
+    int len = 1<<(n-2);
+    if(k<=len) return kthGrammar(n-1, k);
+    else return 1-kthGrammar(n-1, k-len);
+    }
+};
