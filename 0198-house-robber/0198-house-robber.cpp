1936058a170c4b2e467ca060428315ee45e1954f
Shubham kashyap <110350667+Shubhamkashyap1601@users.noreply.github.com>
2023-11-12 23:42:22 +0530

    Time: 3 ms (36.35%), Space: 8 MB (82.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,11 +4,20 @@ public:
         int n = nums.size();
         if(n==1)
         return nums[0];
-        vector<int> v(n+1,0);
-        v[1] = nums[0];
-        for(int i=2;i<=n;i++){
-            v[i] = max(v[i-1],v[i-2]+nums[i-1]);
+        // vector<int> v(n+1,0);
+        // v[1] = nums[0];
+        // for(int i=2;i<=n;i++){
+        //     v[i] = max(v[i-1],v[i-2]+nums[i-1]);
+        // }
+        // return v[n];
+        int maxx = 0;
+        int prev2 = 0,prev1 = nums[0];
+        for(int i=1;i<n;i++){
+            cout<<maxx<<" "<<prev1<<" "<<prev2<<endl;
+            maxx = max(prev1,prev2+nums[i]);
+            prev2 = prev1;
+            prev1 = maxx; 
         }
-        return v[n];
+        return maxx;
     }
 };
