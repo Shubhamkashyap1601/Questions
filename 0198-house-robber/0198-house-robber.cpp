c796c551970c6d737fb6763810f52dfe50292e9a
Shubham kashyap <110350667+Shubhamkashyap1601@users.noreply.github.com>
2023-11-12 23:43:20 +0530

    Time: 3 ms (36.35%), Space: 7.9 MB (82.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -7,7 +7,7 @@ public:
         int maxx = 0;
         int prev2 = 0,prev1 = nums[0];
         for(int i=1;i<nums.size();i++){
-            cout<<maxx<<" "<<prev1<<" "<<prev2<<endl;
+            // cout<<maxx<<" "<<prev1<<" "<<prev2<<endl;
             maxx = max(prev1,prev2+nums[i]);
             prev2 = prev1;
             prev1 = maxx; 
