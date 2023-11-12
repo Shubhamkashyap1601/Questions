be3738976bf7da9ae8c2f368159a1dbe57a9c5f2
Shubham kashyap <110350667+Shubhamkashyap1601@users.noreply.github.com>
2023-11-12 23:51:25 +0530

    Time: 4 ms (22.89%), Space: 7.9 MB (96.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,12 +3,13 @@ public:
     int rob(vector<int>& nums){
         if(nums.size()==1)
         return nums[0];
+        int maxx = 0;
         int prev2 = 0,prev1 = nums[0];
         for(int i=1;i<nums.size();i++){
-            int maxx = max(prev1,prev2+nums[i]);
+            maxx = max(prev1,prev2+nums[i]);
             prev2 = prev1;
             prev1 = maxx; 
         }
-        return prev1;
+        return maxx;
     }
 };
