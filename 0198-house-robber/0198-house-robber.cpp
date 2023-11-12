3eb92995f5510b1c8115dad40d57dfbe1f1de0b0
Shubham kashyap <110350667+Shubhamkashyap1601@users.noreply.github.com>
2023-11-12 23:58:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (96.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,9 +1,9 @@
+int maxx = 0;
 class Solution {
 public:
     int rob(vector<int>& nums){
         if(nums.size()==1)
         return nums[0];
-        int maxx = 0;
         int prev2 = 0,prev1 = nums[0];
         for(int i=1;i<nums.size();i++){
             maxx = max(prev1,prev2+nums[i]);
