974c6b9d03171563bfcab47dd377ee29f3207c5f
Shubham kashyap <110350667+Shubhamkashyap1601@users.noreply.github.com>
2023-11-12 23:43:13 +0530

    Time: 0 ms (100%), Space: 8 MB (66.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1)
+        // int n = nums.size();
+        if(nums.size()==1)
         return nums[0];
-        // vector<int> v(n+1,0);
-        // v[1] = nums[0];
-        // for(int i=2;i<=n;i++){
-        //     v[i] = max(v[i-1],v[i-2]+nums[i-1]);
-        // }
-        // return v[n];
         int maxx = 0;
         int prev2 = 0,prev1 = nums[0];
-        for(int i=1;i<n;i++){
+        for(int i=1;i<nums.size();i++){
             cout<<maxx<<" "<<prev1<<" "<<prev2<<endl;
             maxx = max(prev1,prev2+nums[i]);
             prev2 = prev1;
